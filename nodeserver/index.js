b4c744855a69ab3af0f65eca2d8303d487c6139e
JesusSanchezPluma <jesus.spluma@gmail.com>
2021-08-02 22:12:02 -0500

    end and setHeader

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -12,8 +12,9 @@ const server = http.createServer( ( request, response ) => {
     // Con qué?
     // Son cosas que ya vienen configuradas en mi request
     response.statusCode = 200 //status que resuelve el servidor al resolver la petición
-    response.write('Hola koders desde el servidor')
-    responde.end()
+    //response.write('Hola koders desde el servidor')
+    response.setHeader('Content-Type', 'text/html')
+    responde.end('<h1>Hello, world!</h1>')
 })
 
 server.listen( 8000, () => {
