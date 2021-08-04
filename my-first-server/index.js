62a66e2a09f5233873427b6ba722479ba82d2f5b
JesusSanchezPluma <jesus.spluma@gmail.com>
2021-08-03 21:07:39 -0500

    nodejs server request y response with postman
    exiut
    exit

--- my-first-server/index.js
+++ my-first-server/index.js
@@ -5,10 +5,30 @@ const http = require('http');
 const server = http.createServer((request, response)=>{
     console.log('path: ', request.url);
     console.log('method: ', request.method)
-    response.write('Hola desde mi servidor node');
+
+    if(request.url == '/mentors' && request.method == 'GET'){
+        response.write('Aqui encontraras a los mentores de kodemia')
+    } else if(request.url == '/mentors' && request.method == 'POST'){
+        response.write('Aqui podrás crear un mentor')
+    } else {
+        response.write('No se esperaba esto')
+    }
+    // response.write('Hola desde mi servidor node ');
     response.end();
 })
 
 server.listen(8080,() =>{
     console.log('Server running un http://localhost:8080');
 })
+
+/*
+
+URL -> /mentors
+
+GET /mentors -> 'Aqui encontraras a los mentores de kodemia'
+
+POST /mentors -> 'Aqui podrás crear un mentor'
+
+-> 'No se esperaba esto'
+
+*/
