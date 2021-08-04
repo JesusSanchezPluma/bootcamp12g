e1b8d8f449820be5019f0edf481bd094dd8d31ac
JesusSanchezPluma <jesus.spluma@gmail.com>
2021-08-03 20:22:50 -0500

    advances in class

--- my-first-server/index.js
+++ my-first-server/index.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 //CreateServer recibe un callback que es request y response, esto es posicional
 
-http.createServer((request, response)=>{
+const server = http.createServer((request, response)=>{
+    console.log('path: ', request.url);
+    console.log('method: ', request.method)
+    response.write('Hola desde mi servidor node');
+    response.end();
+})
 
+server.listen(8080,() =>{
+    console.log('Server running un http://localhost:8080');
 })
